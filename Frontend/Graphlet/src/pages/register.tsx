d29604ef7c7e06c2a9e0c127b46d9a8002b9cfeb
Zsombor Fekete <jatekoszs12@gmail.com>
2026-01-22 22:05:22 +0100

    working registe and login

--- Frontend/Graphlet/src/pages/register.tsx
+++ Frontend/Graphlet/src/pages/register.tsx
@@ -4,26 +4,38 @@ import SuccessfulRegister from "../components/loginregister/successfulRegister.t
 import '../components/loginregister/login.css'
 import {ErrorComponent} from "../components/error/errorComponent.tsx";
 
-export default function Register(){
+export default function Register() {
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(false);
     const navigate = useNavigate();
     const [error, setError] = useState(false);
 
-    function register(){
+    async function registerUser() {
         const emailInput = document.querySelector(".emailInput") as HTMLInputElement | null;
         const passwordInput = document.querySelector(".passwordInput") as HTMLInputElement | null;
         const usernameInput = document.querySelector(".usernameInput") as HTMLInputElement | null;
 
-        if(!emailInput?.value || !passwordInput?.value || !usernameInput?.value){
+        setError(false)
+        if (!emailInput?.value || !passwordInput?.value || !usernameInput?.value) {
             setError(true);
+            console.log(error);
             return;
         }
+        const rawRes = await fetch("http://localhost:5188/api/user/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: usernameInput.value,
+                email: emailInput.value,
+                password: passwordInput.value
+            })
+        })
+        const res = await rawRes.json();
+        console.log(res + "nice")
 
-        // Simulate sending to backend
         setLoading(true);
-
-        console.log(emailInput.value, passwordInput.value, usernameInput.value)
 
         // On successful response: show success immediately, then redirect after a delay
         setSuccess(true);
@@ -34,32 +46,35 @@ export default function Register(){
         }, 1000);
     }
 
-    return(
+    return (
         <section className="register-section fg">
-            <h1 >Register</h1>
-            {success && <SuccessfulRegister />}
+            <h1>Register</h1>
+            {success && <SuccessfulRegister/>}
             <div>
                 <table className="registerForm">
                     <tbody>
-                        <tr>
-                            <td>Email: </td>
-                            <td><input type="email" className="emailInput" required /></td>
-                        </tr>
-                        <tr>
-                            <td>Username: </td>
-                            <td><input type="text" className="usernameInput" required /></td>
-                        </tr>
-                        <tr>
-                            <td>Password: </td>
-                            <td><input type="password" className="passwordInput" required /></td>
-                        </tr>
-                        {error && <ErrorComponent error={"Hiba van, itt valami nem jó!"}/>}
-                        <tr>
-                            <td colSpan={2}><button onClick={register} disabled={loading}>{loading ? 'Working...' : 'Register'}</button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan={2}><a href="/login">Already have an account? Login here!</a></td>
-                        </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><input type="email" className="emailInput" required/></td>
+                    </tr>
+                    <tr>
+                        <td>Username:</td>
+                        <td><input type="text" className="usernameInput" required/></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input type="password" className="passwordInput" required/></td>
+                    </tr>
+                    {error && <ErrorComponent error={"Hiba van, itt valami nem jó!"}/>}
+                    <tr>
+                        <td colSpan={2}>
+                            <button onClick={registerUser}
+                                    disabled={loading}>{loading ? 'Working...' : 'Register'}</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><a href="/login">Already have an account? Login here!</a></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
